aeaab35e341560ed921df2edc2023fb5899696e6
Vudang <vudang@Vudangs-iMac.local>
2020-08-11 10:00:28 +0700

    Setup Bottom Tab Navigation

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ import {NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
+import AntDesin from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './src/screens/HomeScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import NotificationScreen from './src/screens/NotificationScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import CategoryScreen from './src/screens/CategoryScreen';
+
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
@@ -29,6 +34,36 @@ const TabNavigator = () => {
         <Icon name="home" size={26} color={color}/>
       )
     }}
+    />
+    <Tab.Screen
+      name="Category"
+      component={CategoryScreen}
+      options={{
+        tabBarLabel:'Danh mục',
+        tabBarIcon: ({color}) => (
+          <AntDesin name="appstore-o" size={26} color={color}/>
+        )
+      }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={SearchScreen}
+      options={{
+        tabBarLabel:'Tìm kiếm',
+        tabBarIcon: ({color}) => (
+          <Ionicons name="search-outline" size={26} color={color}/>
+        )
+      }}
+    />
+    <Tab.Screen
+      name="Notification"
+      component={NotificationScreen}
+      options={{
+        tabBarLabel:'Thông báo',
+        tabBarIcon: ({color}) => (
+          <Ionicons name="notifications" size={26} color={color}/>
+        )
+      }}
     />
     <Tab.Screen 
     name="Profile" 
---
+++ src/screens/CategoryScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default CategoryScreen = () => {
+  return(
+    <View>
+      <Text>CategoryScreen</Text>
+    </View>
+  );
+}
